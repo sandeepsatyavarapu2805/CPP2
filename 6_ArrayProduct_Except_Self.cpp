467dee11190c3dd12fffa625315897f7016ec07b
sandeepsatyavarapu2805 <sandeepsatyavarapu2805@gmail.com>
2026-06-04 13:12:38 +0530

    modified Q6

--- 6_ArrayProduct_Except_Self.cpp
+++ 6_ArrayProduct_Except_Self.cpp
@@ -20,7 +20,7 @@ vector<int> productExceptSelf(vector<int> &nums)
     int suffix = 1;
     for (int i = n - 1; i >= 0; i--)
     {
-        result[i] = suffix;
+        result[i] *= suffix;
         suffix *= nums[i];
     }
     
