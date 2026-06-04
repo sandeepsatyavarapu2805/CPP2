44b455d1cacad0e1510ed1efad5e36b6f5a4db1c
sandeepsatyavarapu2805 <sandeepsatyavarapu2805@gmail.com>
2026-06-04 13:10:16 +0530

    modified Q6

--- 6_ArrayProduct_Except_Self.cpp
+++ 6_ArrayProduct_Except_Self.cpp
@@ -10,16 +10,20 @@ vector<int> productExceptSelf(vector<int> &nums)
     int n = nums.size();
     vector<int> result(n, 1);
 
-    for (int i = 1; i < n; i++)
+    int prefix = 1;
+    for (int i = 0; i < n; i++)
     {
-        result[i] = result[i - 1] * nums[i - 1];
+        result[i] = prefix;
+        prefix *= nums[i];
     }
 
-    for (int i = n - 2; i >= 0; i--)
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--)
     {
-        result[i] *= result[i - 1] * nums[i];
+        result[i] = suffix;
+        suffix *= nums[i];
     }
-
+    
     return result;
 }
 
